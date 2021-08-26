b88ca6e3576fe207525dc83f6cbdb73b6d0e8aa1
Bombohog <70882697+Bombohog@users.noreply.github.com>
2021-08-26 15:03:15 +0200

    Can end game with q or Q

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -22,22 +23,32 @@ public class Main {
         System.out.println("You want to play a game? yes, no");
         if (input.nextLine().equals("yes")) {
 
+            String inputSTR = "";
+
+            while (true) {
 
                 System.out.println("Write a number: ");
-                try { updateMax(input.nextInt()); } catch (Exception e) { /* looks like something went wrong, but we dont care */ }
+                inputSTR = input.next();
 
-                while (true) {
-
-                    System.out.println("Write another number: ");
-                    try { updateMax(input.nextInt()); } catch (Exception e) { /* looks like something went wrong, but we dont care */ }
-
-                    System.out.println("Average is: " + calculateAverage());
-
+                if (inputSTR.equals("Q") || inputSTR.equals("q")) {
+                    break;
+                } else if (isNumeric(inputSTR)) {
+                    try { updateMax(Integer.valueOf(inputSTR)); } catch (Exception e) { /* looks like something went wrong, but we dont care */ }
+                } else {
+                    System.out.println("Please write a number or 'q'/'Q' if you want to end the game.\n");
                 }
 
+                System.out.println("Average is: " + calculateAverage());
+
+            }
 
         }
 
+    }
+
+    private static boolean isNumeric(String str) {
+        String regexPattern = "[0-9]+[\\.]?[0-9]*";
+        return Pattern.matches(regexPattern, str);
     }
 
     private static void updateMax(int number) {
